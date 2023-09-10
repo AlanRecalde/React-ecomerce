aba10e905084caf420b415ecf07d2ed25613e06b
Alan <alanjulianrecalde@gmail.com>
2023-09-10 19:03:56 -0300

    push

---
+++ src/components/Clase3.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Clase3() {
+  
+  const condicion= true
+  
+  condicion?alert("Verdadero"):alert("Falso")
+  
+  
+  return (
+    <div></div>
+  )
+}
